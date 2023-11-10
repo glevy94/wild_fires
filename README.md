56ed7477085c54f1b90d6715062abfbd5835a085
Grayson Levy <glevy4@hawaii.edu>
2023-11-10 03:20:40 -1000

    initial push
    draft

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wild_fires
+
+Clone repository, make a new branch with yourName_dev
+Open a terminal window in the folder location $ node server.js
+Open browser to 127.0.0.1:3000
---
+++ public/js/graph.js
@@ -0,0 +1,100 @@
+let costData = {}; // To store cost data
+let dataCache = {}; // Cache for storing loaded data
+
+// Function to load cost data
+function loadCostData() {
+    d3.json('/data/supp_costs.json').then(function(data) {
+        // Convert array to a key-value object
+        data.forEach(item => {
+            costData[item.Year] = item.Total;
+        });
+        // Initial update for cost display
+        updateCostDisplay(document.getElementById('yearSlider').value);
+    }).catch(error => console.error('Error loading cost data:', error));
+}
+
+// Function to update the cost display
+function updateCostDisplay(year) {
+    const cost = costData[year];
+    const costElement = document.getElementById('costDisplay');
+
+    if (cost) {
+        costElement.textContent = `$${cost.toLocaleString()}`;
+        costElement.style.fontSize = `${(cost / 20000000)}px`; // Adjust size relative to cost
+    } else {
+        costElement.textContent = 'No data';
+    }
+}
+
+// Function to plot data using Plotly
+function plotData(geojsonData, year) {
+    // Process the GeoJSON data to extract coordinates and other properties
+    var plotData = geojsonData.features.map(function(feature) {
+        return {
+            type: 'scattergeo',
+            mode: 'markers',
+            text: `Fire Size: ${feature.properties.IncidentSize} acres`, // Customize as per your data
+            lon: [feature.geometry.coordinates[0]],
+            lat: [feature.geometry.coordinates[1]],
+            marker: {
+                size: 2,
+                color: 'red' // Customize color as needed
+            }
+        };
+    });
+
+    var layout = {
+        title: `Incident Data for ${year}`,
+        geo: {
+            scope: 'usa',
+            projection: {
+                type: 'albers usa'
+            }
+        },
+        showlegend: false,
+        autosize: true,
+        height: 1000,
+
+    };
+
+    var config = {
+        responsive: true,
+    }
+
+    Plotly.newPlot('graph', plotData, layout, config);
+}
+
+// Function to load data based on year and update the graph
+function updateGraph(year) {
+    // Update the slider value display
+    document.getElementById('slider-value').textContent = year;
+
+    // Use cached data if available
+    if (dataCache[year]) {
+        plotData(dataCache[year], year);
+        return;
+    }
+
+    // Fetch the data using D3.js
+    d3.json(`/data/data_${year}.geojson`).then(function(geojsonData) {
+        dataCache[year] = geojsonData; // Cache the data
+        plotData(geojsonData, year);
+    }).catch(error => {
+        console.error('Error loading the GeoJSON data:', error);
+        // Handle the error appropriately in your application context
+    });
+}
+
+// Event listener for the slider
+document.getElementById('yearSlider').addEventListener('input', function() {
+    updateGraph(this.value);
+    document.getElementById('slider-value').textContent = this.value;
+    updateCostDisplay(this.value);
+});
+
+// Initial graph setup
+const initialYear = document.getElementById('yearSlider').value;
+updateGraph(initialYear);
+document.getElementById('slider-value').textContent = initialYear;
+loadCostData();
+
